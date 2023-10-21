5b2c95a44cf4197a47cd322ce130c81157a4b525
ilml <mytomlong@gmail.com>
2023-10-22 05:45:46 +0800

    add

---
+++ s3dp.py
@@ -0,0 +1,15 @@
+"""
+class definition of simple 3d parallelism
+
+@tom 10/21/2023
+"""
+
+
+class S3DP:
+    def __init__(model, tp: int, mp: int, zero: int):
+        return model
+
+    
+
+    
+    
